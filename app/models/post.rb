eec7d7b6c504da678e8570a2bf30b64f3ee65d47
yellowpad <zayneabraham1@gmail.com>
2017-03-03 10:57:21 -0500

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ActiveRecord::Base
+	validates :title, presence: true
+	validates :content, length: { minimum: 250 }
+	validates :summary, length: { maximum: 250 }
+	validates_inclusion_of :category, :in => ["Fiction", "Non-Fiction"]	
+	validate :is_clickbait?
+
+
+	CLICKY = [
+		/Won't Believe/i, 
+		/Secret/i, 
+		/Top [number]/i, 
+		/Guess/i
+	]
+
+	def is_clickbait?
+		if CLICKY.none? {|pat| pat.match title}
+			errors.add(:title, "Title must be clickbait.")
+		end
+	end
 end
